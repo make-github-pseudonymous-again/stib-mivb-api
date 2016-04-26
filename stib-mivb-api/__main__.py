90237602ca84c6fbd5f54294f140b395b40f00b0
aureooms <aurelien.ooms@gmail.com>
2016-04-26 20:03:42 +0200

    use flask instead of eve

--- stib-mivb-api/__main__.py
+++ stib-mivb-api/__main__.py
@@ -1,7 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
-if __name__ == '__main__' :
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-    from eve import Eve
-
-    app = Eve()
+if __name__ == "__main__":
     app.run()
