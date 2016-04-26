7b226683693c0233923a298fdaf0bf2a9082f81c
aureooms <aurelien.ooms@gmail.com>
2016-04-26 20:39:49 +0200

    properly set port and host

--- stib-mivb-api/__main__.py
+++ stib-mivb-api/__main__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(port=7000)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
