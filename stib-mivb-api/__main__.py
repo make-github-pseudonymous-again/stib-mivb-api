b7655111511d1f12589b0643ab8a025600c812cd
aureooms <aurelien.ooms@gmail.com>
2016-04-29 01:59:47 +0200

    fix

--- stib-mivb-api/__main__.py
+++ stib-mivb-api/__main__.py
@@ -285,7 +285,7 @@ def app_route_realtime_stop(id = None):
     output['results'] = results
     output['sources'] = { halt : REQUEST.format(halt) for halt in halts }
     root = request.host_url.rstrip('/')
-    output['url'] = root + url_for('app_route_realtime_stop', id = id) ,
+    output['url'] = root + url_for('app_route_realtime_stop', id = id)
 
     headers = { 'Cache-Control' :  'no-cache' }
 
